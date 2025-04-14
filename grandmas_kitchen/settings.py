20c9a29bb11c742235aa3bcc3309a3b3e142868f
vamshibillakanti <151733805+vamshibillakanti@users.noreply.github.com>
2025-04-15 01:06:31 +0530

    Fix static and media file config for Render

--- grandmas_kitchen/settings.py
+++ grandmas_kitchen/settings.py
@@ -134,3 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Only add this if DEBUG = False
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
