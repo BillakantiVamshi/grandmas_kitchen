3f1ec44b596d64cc73c0c24c43a0ecfa1b39d08d
vamshibillakanti <151733805+vamshibillakanti@users.noreply.github.com>
2025-04-16 17:05:47 +0530

    settings changed with db

--- grandmas_kitchen/settings.py
+++ grandmas_kitchen/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -92,6 +94,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.parse("postgresql://grandmas_kitchen_db_user:D1sTHYAkXlOEq2hunxKTLe7zP9AyYcQn@dpg-cvvp013e5dus73cgpvmg-a.oregon-postgres.render.com/grandmas_kitchen_db")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
