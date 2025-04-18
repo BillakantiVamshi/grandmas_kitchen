cb8470d9404e84e3ab1b42dcdd2349f0b2360039
vamshibillakanti <151733805+vamshibillakanti@users.noreply.github.com>
2025-04-18 17:23:23 +0530

    Fix create_admin for custom user model

--- create_admin.py
+++ create_admin.py
@@ -1,18 +1,17 @@
-# create_admin.py
-
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grandmas_kitchen.settings")  # change 'your_project_name'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grandmas_kitchen.settings")  # replace with your actual project name
 django.setup()
 
-from django.contrib.auth.models import User
+from app1.models import Account  # This is your custom user model
 
 username = "Vamshi@admin"
 password = "rishi123@"
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email="", password=password)
+if not Account.objects.filter(username=username).exists():
+    Account.objects.create_superuser(username=username, email="", password=password)
     print("Superuser created.")
 else:
     print("Superuser already exists.")
+    
